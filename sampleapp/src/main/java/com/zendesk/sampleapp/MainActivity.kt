7f0bffa398532fc1a3d2b424c368b3542399cc21
danthedrummer <danthedrummerrof@gmail.com>
2018-06-19 10:29:41 +0100

    Introducing Sample App
    Initial creation of sample app with 2 functions: identify and track. I
    will be adding features to this sample app to display all of the
    available features in the near future.
    
    This commit also includes some fixes for a push notification bug that
    was introduced with the safety improvements added previously. These safe
    calls prevented an expected exception from occurring so the small icon
    was never correctly set causing the notification to be swallowed by the
    system.

---
+++ sampleapp/src/main/java/com/zendesk/sampleapp/MainActivity.kt
@@ -0,0 +1,75 @@
+package com.zendesk.sampleapp
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import io.outbound.sdk.Event
+import io.outbound.sdk.Outbound
+import io.outbound.sdk.User
+import kotlinx.android.synthetic.main.activity_main.*
+
+const val LOG_TAG = "SampleMainActivity"
+
+const val NOTIFICATION_CHANNEL_ID = "outbound_sample_app_notifications"
+const val NOTIFICATION_CHANNEL_NAME = "Outbound Sample App Notifications"
+
+//TODO: Replace with your Connect private api key
+const val CONNECT_API_KEY: String = "xxx"
+
+//TODO: Replace with your Firebase Google Project ID
+const val GOOGLE_PROJECT_ID: String = "xxx"
+
+class MainActivity : AppCompatActivity() {
+
+    private val sampleUser = User.Builder()
+            .setUserId("th3_warth0g")
+            .setFirstName("Frank")
+            .setLastName("Reynolds")
+            .build()
+
+    private val sampleEvent = Event("Sample Event").apply {
+        setUserId(sampleUser.userId)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(NOTIFICATION_CHANNEL_ID,
+                    NOTIFICATION_CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT)
+            notificationChannel.enableVibration(true)
+
+            val notificationManager =
+                    getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(notificationChannel)
+
+            Outbound.init(this.application, CONNECT_API_KEY,
+                    GOOGLE_PROJECT_ID, NOTIFICATION_CHANNEL_ID)
+        } else {
+            Outbound.init(this.application, CONNECT_API_KEY, GOOGLE_PROJECT_ID)
+        }
+
+        identify_user_button.setOnClickListener {
+            identifyUser(sampleUser)
+        }
+
+        track_event_button.setOnClickListener {
+            trackEvent(sampleEvent)
+        }
+
+    }
+
+    private fun identifyUser(user: User) {
+        Log.d(LOG_TAG, "Identifying User")
+        Outbound.identify(user)
+    }
+
+    private fun trackEvent(event: Event) {
+        Log.d(LOG_TAG, "Tracking event")
+        Outbound.track(event)
+    }
+}
