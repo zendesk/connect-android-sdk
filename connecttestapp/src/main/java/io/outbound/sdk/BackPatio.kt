54f651416a1ca3afcb65e51c1318eb305ba2f5eb
danthedrummer <danthedrummerrof@gmail.com>
2018-07-19 09:22:31 +0100

    Project Cleanup
    There were some warnings that were popping up from Gradle every time the
    project was built. I cleaned up some unused clutter from the build files
    and some problematic/unused dependencies. Firebase was a prime culprit
    as it kept giving out about requiring the core library but it has
    mismatched versions with messaging. Removed some more GCM remnants from
    the sample app and deleted an empty test directory from the sdk module.
    I also had to regenerate the gradle wrapper as it was somehow not
    executable anymore and chmod didn't fix it.

--- connecttestapp/src/main/java/io/outbound/sdk/BackPatio.kt
+++ connecttestapp/src/main/java/io/outbound/sdk/BackPatio.kt
@@ -2,7 +2,6 @@ package io.outbound.sdk
 
 import android.app.Application
 
-fun initSdkForTesting(app: Application, apiKey: String, gcmSenderId: String,
-                      notificationChannelId: String?, testUrl: String) {
-    Outbound.initForTesting(app, apiKey, gcmSenderId, notificationChannelId, testUrl)
+fun initSdkForTesting(app: Application, apiKey: String, notificationChannelId: String?, testUrl: String) {
+    Outbound.initForTesting(app, apiKey, notificationChannelId, testUrl)
 }
